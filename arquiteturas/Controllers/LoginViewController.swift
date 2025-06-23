ac0db3c5045721be617edaafce1f4da4e6655cb0
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2025-06-23 15:58:12 -0300

    add login view controller

--- arquiteturas/Controllers/LoginViewController.swift
+++ arquiteturas/Controllers/LoginViewController.swift
@@ -6,3 +6,43 @@
 //
 
 import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        let manager = UserManager(business: UserBusiness())
+        
+        if let email = emailTextField.text,
+           let password = passwordTextField.text {
+            manager.login(email: email, password: password) { userModel in
+                self.openHomeView()
+            } failureHandler: {error in
+                self.showMessage(title: "Erro", message: error?.localizedDescription ?? "")
+          }
+        }
+    }
+    
+    
+    @IBAction func registerButtonTapped(_ sender: Any) {
+        
+    }
+    
+    func showMessage(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true)
+    }
+    
+    func openHomeView() {
+        let homeView = self.storyboard?.instantiateViewController(withIdentifier: "homeViewController") as! HomeViewController
+        homeView.modalPresentationStyle = .fullScreen
+        self.present(homeView, animated: true)
+    }
+    
+}
